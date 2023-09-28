4e08003ec3b80055e474afc2e53698c2d5a2ace5
Rehat Kathuria <hello@reh.at>
2023-09-28 14:11:18 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Networking",
+	platforms: [
+		.iOS(.v17),
+		.macOS(.v14)
+	],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Networking",
+            targets: ["Networking"]),
+    ],
+	dependencies: [
+		.package(
+			url: "https://github.com/apollographql/apollo-ios.git", from: "1.5.0"
+		),
+	],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Networking",
+			dependencies: [
+				.product(name: "Apollo", package: "apollo-ios")
+			]
+		),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]),
+    ]
+)
